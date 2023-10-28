5779d27c01d83810d7c36006d8b220a1b23c8d03
lgf4591 <lgf4591@outlook.com>
2023-10-28 22:43:58 +0800

    test

--- test/test.ps1
+++ test/test.ps1
@@ -39,6 +39,27 @@
 # }
 
 
-Write-Host "mkdir a,b"
-# mkdir a,b
-Pause
+# Write-Host "mkdir a,b"
+# # mkdir a,b
+# Pause
+
+
+function test_ping($iplist)
+{
+    foreach ($myip in $iplist)
+    {
+        $strQuery = "select * from win32_pingstatus where address = '$myip'"
+        # 利用 Get-WmiObject 送出 ping 的查詢
+        $wmi = Get-WmiObject -query $strQuery
+        if ($wmi.statuscode -eq 0) 
+        {
+            return "Pinging`t$myip...`tsuccessful"
+        }
+        else 
+        {
+            return "Pinging`t$myip...`tErrorCode:" + $wmi.statuscode
+        }
+    }
+}
+
+test_ping args[0]
